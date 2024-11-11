68843c0c5dde93bdc1fa1371adaa2f037f237041
Rajasekhar-Sirivella <rajasekhar.sirivella@tcs.com>
2024-11-11 18:08:04 +0000

    Add files via upload

---
+++ text-to-sql/SQL_demo.py
@@ -0,0 +1,97 @@
+import streamlit as st
+from pathlib import Path
+from langchain_community.agent_toolkits.sql.base import create_sql_agent
+from langchain_community.utilities import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+
+st.set_page_config(page_title="Chat with SQL DB")
+st.title("Chat with SQL DB")
+
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt=["Use SQLLite 3 Database- Products.db","Connect to you MySQL Database"]
+
+selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+else:
+    db_uri=LOCALDB
+
+#choose llm using selectbox
+llm_choice=st.sidebar.selectbox("Choose the LLM", options=["Groq", "Gemini"])
+if llm_choice=="Groq":
+    llm=ChatGroq(temperature=0.4, model_name="Llama3-8b-8192")
+    st.sidebar.info("Groq LLM is selected")
+elif llm_choice =="Gemini":
+    llm = ChatGoogleGenerativeAI(temperature=0.5, model='gemini-1.5-pro')
+    st.sidebar.info("Gemini LLM is selected")
+
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"product.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+    
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+
+agent=create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query=st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback=StreamlitCallbackHandler(st.container())
+        response=agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
+
+        
+
+
