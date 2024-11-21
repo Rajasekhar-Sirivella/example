a5bc4fce0b4bfd442dc534adb02554807816415d
Rajasekhar-Sirivella <rajasekhar.sirivella@tcs.com>
2024-11-21 10:59:15 +0000

    Create react_agent.py

---
+++ text-to-sql/react_agent.py
@@ -0,0 +1,154 @@
+import streamlit as st
+from pathlib import Path
+from langchain_community.utilities import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from langgraph.prebuilt import create_react_agent
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_vertexai.chat_models import ChatVertexAI
+from google.cloud import aiplatform
+
+PROJECT_ID = "tcs-cto-retail"
+aiplatform.init(project=PROJECT_ID, location="us-central1")
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+
+st.set_page_config(page_title="Chat with SQL DB")
+st.title("Chat with SQL DB")
+
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt=["Use SQLLite 3 Database- Products.db","Connect to you MySQL Database"]
+
+selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+else:
+    db_uri=LOCALDB
+
+#choose llm using selectbox
+llm_choice=st.sidebar.selectbox("Choose the LLM", options=["VertexAI", "Groq", "Gemini"])
+if llm_choice=="VertexAI":
+    llm = ChatVertexAI(
+        model="gemini-1.5-flash-002",
+        temperature=0.4)
+    st.sidebar.info("VertexAI LLM is selected")
+elif llm_choice=="Groq":
+    llm=ChatGroq(temperature=0.4, model_name="Llama3-8b-8192")
+    st.sidebar.info("Groq LLM is selected")
+elif llm_choice =="Gemini":
+    llm = ChatGoogleGenerativeAI(temperature=0.5, model='gemini-1.5-pro')
+    st.sidebar.info("Gemini LLM is selected")
+
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"olist.sqlite").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+    
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+
+SQL_PREFIX = """You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
+You can order the results by a relevant column to return the most interesting examples in the database.
+Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+You have access to tools for interacting with the database.
+Only use the below tools. Only use the information returned by the below tools to construct your final answer.
+You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+To start you should ALWAYS look at the tables in the database to see what you can query.
+Do NOT skip this step.
+Then you should query the schema of the most relevant tables."""
+
+system_message = SystemMessage(content=SQL_PREFIX)
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+tools = toolkit.get_tools()
+
+agent_executor = create_react_agent(llm, tools, state_modifier=system_message)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "query": "", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    if msg['role']=="user":
+        st.chat_message(msg["role"]).write(msg["content"])
+    elif msg['role']=='assistant' and msg["query"]!="":
+        with st.chat_message("assistant"):
+            st.write(msg['query'])
+            st.write(msg['content'])
+    else:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+user_query=st.chat_input(placeholder="Ask anything from the database")
+
+def get_metadata(query):
+    try:
+        # Connect to SQLite database
+        conn = sqlite3.connect("olist.sqlite")
+        conn.row_factory = sqlite3.Row  # Use Row factory to make rows indexable by column name
+        cursor = conn.cursor()
+        
+        # Execute the query
+        cursor.execute(query)
+        
+        # Get metadata
+        columns = [description[0] for description in cursor.description]
+        data = cursor.fetchall()
+        
+        # Format data as a list of dictionaries
+        formatted_data = [dict(zip(columns, row)) for row in data]
+        # {"columns": columns, "data": formatted_data}
+        df = pd.DataFrame(formatted_data)
+        return df
+    except Exception as e:
+        return {"error": str(e)}
+
+if user_query:
+    try:
+        st.session_state.messages.append({"role": "user", "content": user_query})
+        st.chat_message("user").write(user_query)
+        with st.chat_message("assistant"):
+            response=agent_executor.invoke({"messages": [HumanMessage(content=user_query)]})
+            #query=(response["messages"][-3].additional_kwargs['function_call']['arguments']) #returns string format
+            query=response["messages"][-3].tool_calls[0]['args'] #returns json format
+            result="Output : "+response["messages"][-2].content+"\n\n\n"+response["messages"][-1].content #query response and llm response
+            st.session_state.messages.append({"role":"assistant", "query": query,"content": result})
+            st.write(query)
+            st.write(result)
+            st.table(get_metadata(query = query["query"]))
+    except Exception as e:
+        st.error("Error Occured... Try again.")
+        print("The error is: ",e)
