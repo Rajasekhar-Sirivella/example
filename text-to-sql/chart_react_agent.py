72d419e17207f799aa87d1b6482aecf797ca43b6
Rajasekhar-Sirivella <rajasekhar.sirivella@tcs.com>
2024-11-22 09:17:00 +0000

    Update chart_react_agent.py

--- text-to-sql/chart_react_agent.py
+++ text-to-sql/chart_react_agent.py
@@ -1,26 +1,21 @@
 import streamlit as st
 from pathlib import Path
-from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.utilities import SQLDatabase
-from langchain.agents.agent_types import AgentType
-from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_google_genai import ChatGoogleGenerativeAI
 from sqlalchemy import create_engine
 import sqlite3
 import os
 from dotenv import load_dotenv
-import matplotlib.pyplot as plt
-import re
+# import matplotlib.pyplot as plt
+import pandas as pd
+# import seaborn as sb
+# import re
 from langchain_core.messages import SystemMessage, HumanMessage
 from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
-
-# from langchain_openai import AzureChatOpenAI
 from langchain_groq import ChatGroq
-# from openai import AzureOpenAI
-from audio_recorder_streamlit import audio_recorder
 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
@@ -30,28 +25,6 @@ from google.cloud import aiplatform
 
 PROJECT_ID = "tcs-cto-retail"
 aiplatform.init(project=PROJECT_ID, location="us-central1")
-
-
-# os.environ["AZURE_OPENAI_ENDPOINT"] = "https://documentsummary.openai.azure.com/"
-# os.environ["AZURE_OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_API_KEY")
-
-# # Whisper config
-# azure_endpoint_whisper = "https://whisper-tts.openai.azure.com/"
-# azure_key_whisper = os.getenv("WHISPER_OPENAI_API_KEY")
-
-# Speech to text
-# def transcribe_audio(audio_path):
-#     client = AzureOpenAI(
-#         api_key=azure_key_whisper,
-#         api_version="2024-02-01",
-#         azure_endpoint=azure_endpoint_whisper
-#     )
-#     with open(audio_path, "rb") as audio_file:
-#         transcript = client.audio.transcriptions.create(
-#             model="whisper",
-#             file=audio_file,
-#         )
-#     return transcript.text
 
 st.set_page_config(page_title="SQLDB", page_icon=":robot:")
 st.title("Chat with any SQL DB")
@@ -94,7 +67,7 @@ if not db_uri:
 @st.cache_resource(ttl="2h")
 def configure_db(db_uri, mysql_host=None, mysql_user=None, mysql_password=None, mysql_db=None):
     if db_uri == LOCALDB:
-        dbfilepath = (Path(__file__).parent / "product.db").absolute()
+        dbfilepath = (Path(__file__).parent / "olist.sqlite").absolute()
         creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
         return SQLDatabase(create_engine("sqlite://", creator=creator))
     elif db_uri == MYSQL:
@@ -146,73 +119,51 @@ for msg in st.session_state.messages:
 
 # Function to generate a dynamic plot
 def plot_dynamic(data, plot_type):
-    fig, ax = plt.subplots()
-    
     if plot_type == "pie":
-        ax.pie(data['values'], labels=data['labels'], autopct='%1.1f%%', startangle=90)
-        ax.axis('equal')
+        print("continue.....")
     elif plot_type == "bar":
-        ax.bar(data['labels'], data['values'], color='blue')
-        ax.set_xlabel('Labels')
-        ax.set_ylabel('Values')
+        st.bar_chart(data)
     elif plot_type == "line":
-        ax.plot(data['labels'], data['values'], marker='o')
-        ax.set_xlabel('Labels')
-        ax.set_ylabel('Values')
+        st.line_chart(data)
     else:
         st.error("Unsupported plot type!")
         return
-    
-    st.pyplot(fig)
 
 # Function to parse the response and generate a dynamic plot if needed
-def handle_dynamic_response(response):
-    if any(kw in response.lower() for kw in ["plot", "chart", "graph"]):
+def handle_dynamic_response(data, user_query):
+    if any(kw in user_query.lower() for kw in ["plot", "chart", "graph"]):
         # Identify plot type
-        if "pie chart" in response.lower():
+        if "pie chart" in user_query.lower():
             plot_type = "pie"
-        elif "bar chart" in response.lower():
+        elif "bar chart" in user_query.lower():
             plot_type = "bar"
-        elif "line chart" in response.lower():
+        elif "line chart" in user_query.lower():
             plot_type = "line"
         else:
             plot_type = None
-        
         # Extract data
-        matches = re.findall(r"([\w\s]+):\s*(\d+)", response)
-        if matches:
-            labels = [match[0].strip() for match in matches]
-            values = [int(match[1]) for match in matches]
-            data = {"labels": labels, "values": values}
+        if not data.empty:
             plot_dynamic(data, plot_type)
         else:
             st.error("Could not extract chart data from the response.")
     else:
         st.write(response)
 
-# Record audio
-# st.write("Record your query:")
-# recorded_audio = audio_recorder()
-# if recorded_audio:
-#     st.write("Audio recorded successfully!")
-#     audio_file = "recorded_audio.mp3"
-#     with open(audio_file, "wb") as f:
-#         f.write(recorded_audio)
+def get_metadata(query):
+    try:
+        # Connect to SQLite database
+        conn = sqlite3.connect("olist.sqlite")
+        conn.row_factory = sqlite3.Row  # Use Row factory to make rows indexable by column name
+        cursor = conn.cursor()
+        cursor.execute(query)
+        columns = [description[0] for description in cursor.description]
+        data = cursor.fetchall()
+        formatted_data = [dict(zip(columns, row)) for row in data]
+        df = pd.DataFrame(formatted_data)
+        return df
+    except Exception as e:
+        return {"error": str(e)}
 
-#     transcribe_text = transcribe_audio(audio_file)
-#     st.write("Transcribed Text:", transcribe_text)
-
-#     # Append transcribed text to session state and process
-#     if transcribe_text:
-#         st.session_state.messages.append({"role": "user", "content": transcribe_text})
-#         st.chat_message("user").write(transcribe_text)
-#         with st.chat_message("assistant"):
-#             streamlit_callback = StreamlitCallbackHandler(st.container())
-#             response = agent.run(transcribe_text, callbacks=[streamlit_callback])
-
-#             # Handle response, either plot or text
-#             handle_dynamic_response(response)
-#             st.session_state.messages.append({"role": "assistant", "content": response})
 
 # Handle text input
 user_query = st.chat_input(placeholder="Ask me anything from DB!")
@@ -226,12 +177,14 @@ if user_query:
             #query=(response["messages"][-3].additional_kwargs['function_call']['arguments']) #returns string format
             query=response["messages"][-3].tool_calls[0]['args'] #returns json format
             output=response["messages"][-2].content
-            # Handle response, either plot or text
-            handle_dynamic_response(response["messages"][-2].content)
+            data=get_metadata(query["query"])
             result="Output : "+output+"\n\n\n"+response["messages"][-1].content #query response and llm response
             st.session_state.messages.append({"role":"assistant", "query": query,"content": result})
             st.write(query)
             st.write(result)
+            st.table(data)
+            # Handle response, either plot or text
+            handle_dynamic_response(data, user_query)
     except Exception as e:
         st.error("Error Occured... Try again.")
         print("The error is: ",e)
